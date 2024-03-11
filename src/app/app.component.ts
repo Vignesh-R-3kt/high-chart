caee8989fb22d1285779fb15586ee7cd408fd295
Vignesh-R-3kt <vignesh.r@3ktechnologies.com>
2024-03-11 17:32:50 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,70 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import * as Highcharts from 'highcharts';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'high-chart';
+export class AppComponent implements OnInit {
+
+  chartData: any = [];
+  serverData: any = [];
+  currentActive: number = 0;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.http.get('http://localhost:3000/data').subscribe((res: any) => {
+      this.serverData = res;
+      this.organizeData(0);
+    })
+  }
+
+  organizeData(id: number) {
+    this.currentActive = id;
+    const singleItem = this.serverData[id];
+    const customChartData: any[] = [];
+    for (let ele in singleItem) {
+      if (isNaN(singleItem[ele])) {
+        delete singleItem[ele];
+      } else {
+        const data = {
+          name: ele, type: 'bar', data: [Number(singleItem[ele])]
+        }
+        customChartData.push(data);
+      }
+    }
+    this.chartData = customChartData;
+    this.initializechart();
+  }
+
+  initializechart() {
+    const options: Highcharts.Options = {
+      chart: {
+        type: 'column'
+      },
+      accessibility: {
+        enabled: false
+      },
+      title: {
+        text: 'Data Visualize'
+      },
+      xAxis: {
+        title: {
+          text: 'Values'
+        }
+      },
+      yAxis: {
+        title: {
+          text: 'Range'
+        }
+      },
+      series: this.chartData
+    };
+
+    Highcharts.chart('container', options);
+  }
+
 }
